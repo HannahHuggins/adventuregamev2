cbf9f2ef8744c39bad70057926190d22746524e9
Hannah huggins <dowdallhannah@yahoo.com>
2018-11-26 15:40:31 +0000

    Created functions and used a dictionary to map the different scenes together.

--- engine.py
+++ engine.py
@@ -1,4 +1,7 @@
+from sys import exit
 from random import randint
+from textwrap import dedent
+
 
 class Scene(object):
 
@@ -27,7 +30,7 @@ class Engine(object):
 class Death(Scene):
 
     quips = [
-        "You died you sucka",
+        "You became a coopian you sucka",
         "Your Mum needs a comma",
         "You died you dickhead",
     ]
@@ -40,38 +43,149 @@ class Death(Scene):
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print("-" * 10)
+        print(dedent("""
+        The coopians of Planet coop have invaded your workplace and annihilated your
+        colleagues. You have been forced to adopt their way of life and their 
+        confusing agile principles. You mission is to remain as introverted as possible,
+        to avoid turning into a coopian. 
+        
+        The project manager is a tall, green-haired alien with horns protruding from his 
+        forehead. He screams "stand up!" and you watch as the other coopians flock to join him.
+        
+        Do you:
+        
+        a.) Join them cheerily
+        b.) Hide
+        c.) Pretend as if nothing has happened
+        """))
+
+        choice = input("> ")
+
+        if choice == "join":
+            return 'death'
+        elif choice == "hide":
+            print(dedent("""
+            You hide under your desk like a coward, listening out for the signs of 
+            footsteps. 
+            The manager walks around the corner and glares at you. You avert your eyes and
+            start humming a song under your breath. He sighs and turns away. You will never be a true coopian.
+            """))
+            return 'device_lab'
+        elif choice == "pretend":
+            print(dedent("""
+            You follow the horde into the auditorium and see that they are taking part in a satanic ritual
+            known as a "retro". You look on in horror as they all share ideas and "take part." You don't know
+            where to look.
+            """))
+            return 'auditorium'
+        else:
+            print("Please try again.")
+            return 'central_corridor'
 
 
-class LaserWeaponArmory(Scene):
+class DeviceLab(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        You emerge from under your desk and run to the device lab, scanning the rows of desks for any sign
+        of the coopians. 
+        You fling open the door to the lab and grab the nearest ipad to use as a weapon.
+        You see a coopian walk towards you and whap them in the side of the head with apples latest device.
+        "You'll never convert me!" You shout.
+        """))
+        return 'fight'
 
 
-class TheBridge(Scene):
+class Auditorium(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        After what feels like a week (2 hours in coopian time), they start to leave the auditorium.
+        You breathe a sign of relief, you could feel your resolve breaking.
+        "They're not so bad" you find yourself thinking.
+        All of a sudden, the green haired project manager is behind you.
+        "What did you think of the session?" He asked with a friendly smile.
+        You scramble for something to say.
+         """))
+        return 'awkward'
 
 
-class EscapePod(Scene):
+class Fight(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+        You sprint to the door of the device room, to find that it's locked. Panic rises in your throat
+        as you claw at the door. You notice a keypad on the side of the door. The passcode is 3 digits, but you
+        can't remember what it is. You remember being told that if you input the code incorrectly 10 times the door
+        will remain shut and the alarms will sound, giving away your position.
+        """))
+
+        code = randint(1,5)
+        guess = int(input("> "))
+        guesses = 0
+
+        while guess != code and guesses < 9:
+            print("Try again!")
+            guesses += 1
+            guess = int(input())
+        if guesses == 10:
+            return 'death'
+        elif guess == code:
+            print(dedent("""
+            You grab an iphone in each hand, and connect the charger cables to them, using them as nunchucks.
+                They whistle as they swing through the air, knocking out the remaining coopians.
+                You survived!
+            
+            """))
+            return 'last_scene'
+
+            # if guess > code:
+            #     print("Try guessing lower")
+            #     guesses.append(1)
+            # elif guess < code:
+            #     print("Try guessing higher")
+
+
+class Awkward(Scene):
+
+    def enter(self):
+        print(dedent("""
+        "It was great." You whisper, sweat forming on your brow. He senses your hesitation and moves closer.
+        His red eyes pierce yours, and smoke starts billowing out of his dragon-like nose. 
+        You run to the device lab, knowing that this is your only hope.
+        """))
+        return 'device_lab'
+
+
+class TheEnd(Scene):
+
+    def enter(self):
+        print("You have finished the game!")
+        exit(0)
 
 
 class Map(object):
 
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'death': Death(),
+        'device_lab': DeviceLab(),
+        'auditorium': Auditorium(),
+        'fight': Fight(),
+        'awkward': Awkward(),
+        'last_scene': TheEnd(),
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
-
+        return self.next_scene(self.start_scene)
 
 
 a_map = Map('central_corridor')
